794d44915e0d9b11fcd3f21334bd7c0e105a7c9f
Kim199610 <kk2247401@naver.com>
2025-02-06 19:03:36 +0900

    인벤토리 보기 구현

--- SpartaDungeon/SpartaDungeon/Program.cs
+++ SpartaDungeon/SpartaDungeon/Program.cs
@@ -5,16 +5,65 @@
         static void Main(string[] args)
         {
             GameManager gm = new GameManager();
-
+            
+            gm.InventoryScreen();
+            
         }
     }
 
     class GameManager
     {
-        public GameManager()
+        List<EquipItem> equipItemList;
+        public GameManager()        //  tring name, EquipType type, int atk, int def, int maxHp, string discription, int cost
+        {
+            equipItemList = new List<EquipItem>
+            {
+            new EquipItem("수련자의 갑옷", EquipType.Amor, 0,2,50, "수련에 도움을 주는 갑옷입니다. ", 1000),
+            new EquipItem("무쇠갑옷", EquipType.Amor, 0,4,100, "무쇠로 만들어져 튼튼한 갑옷입니다. ", 2000),
+            new EquipItem("스파르타의 갑옷", EquipType.Amor, 0,8,200, "스파르타의 전사들이 사용했다는 전설의 갑옷입니다. ", 3500),
+            new EquipItem("낡은 검", EquipType.Weapon, 5,0,0, "쉽게 볼 수 있는 낡은 검 입니다. ", 600),
+            new EquipItem("청동 도끼", EquipType.Weapon, 10,0,0, "어디선가 사용됐던거 같은 도끼입니다. ", 1500),
+            new EquipItem("스파르타의 창", EquipType.Weapon, 20,0,0, "스파르타의 전사들이 사용했다는 전설의 창입니다. ", 2500),
+            //아이템 추가 부분
+            new EquipItem("추가 아이템 1", EquipType.Weapon, 0,2,0, "스파르타의 전사들이 사용했다는 전설의 창입니다. ", 2500),
+            new EquipItem("추가 아이템 2", EquipType.Weapon, 0,4,0, "스파르타의 전사들이 사용했다는 전설의 창입니다. ", 2500)
+            };
+        }
+        public void InventoryScreen()
+        {
+            Console.Clear();
+            Console.WriteLine("인벤토리");
+            Console.WriteLine("보유중인 아이템을 관리할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("[장비 아이템 목록]");
+
+            for (int i = 0; i <equipItemList.Count; i++)
+            {
+                if (equipItemList[i].isEquip) equipItemList[i].DisplayEquipItem();
+            }
+            Console.WriteLine() ;
+            for (int i = 0;i <equipItemList.Count; i++)
+            {
+                if(equipItemList[i].ItemCount>0) equipItemList[i].DisplayinventoryItem();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("1.장착관리");
+            Console.WriteLine("2.소비품 아이템 보기");
+            Console.WriteLine("0.나가기");
+            Console.WriteLine();
+            switch (Utility.GetInput(0, 2))
+            {
+                case 0: /*MainScreen()*/ break;
+                case 1: EquipScreen(); break;
+                case 2: /*소비품인벤토리Screen();*/break;
+            }
+        }
+        public void EquipScreen()
         {
 
         }
+
     }
 }
 
