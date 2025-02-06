b89b240e577eaf0a4022b852253ce6f537b19f08
kdh5790 <k_0106@naver.com>
2025-02-06 18:04:44 +0900

    [ Feat ] DataManager 클래스 생성 / 캐릭터 생성 함수 추가

---
+++ SpartaDungeon/SpartaDungeon/DataManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaDungeon
+{
+    internal class DataManager
+    {
+        public Player CreateCharacter()
+        {
+
+            Player user = new Player();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine($"스파르타 던전에 오신 여러분 환영합니다.");
+                Console.WriteLine($"사용하실 이름을 입력해주세요.");
+                Console.WriteLine($"");
+                Console.Write($">>");
+                string name = Console.ReadLine(); // 닉네임 설정
+                Console.WriteLine($"");
+
+                if (name != string.Empty && name != null)
+                {
+                    Console.WriteLine($"입력하신 이름은 {name} 입니다. 이대로 진행하시겠습니까?");
+                    Console.WriteLine($"");
+                    Console.WriteLine($"1. 저장");
+                    Console.WriteLine($"2. 다시 설정");
+
+                    int selectNum = Utility.GetInput(1, 2);
+
+                    switch (selectNum)
+                    {
+                        case 1:
+                            user.name = name;
+                            return user;
+                        case 2:
+                            continue;
+                    }
+                }
+            }
+        }
+
+        public void SaveData()
+        {
+
+        }
+
+        public void LoadData()
+        {
+
+        }
+    }
+}
