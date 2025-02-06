c49871fd9d0780a3f1ab29a6e6685f18df60d8ea
SimHyungjin <tlagud26@naver.com>
2025-02-06 17:00:30 +0900

    Create class Dungeon,Monster

---
+++ SpartaDungeon/SpartaDungeon/Monster.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaDungeon
+{
+    public class Monster
+    {
+        public string Name { get; }
+        public int Health { get; set; }
+        public int Atk {  get; set; }
+    }
+}
