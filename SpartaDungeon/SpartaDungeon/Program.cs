0bcb7f5244bbe99f2ce29f84ce0fa31cd9c5742c
Kim199610 <kk2247401@naver.com>
2025-02-06 20:13:37 +0900

    상점 구현중 퇴근

--- SpartaDungeon/SpartaDungeon/Program.cs
+++ SpartaDungeon/SpartaDungeon/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace SpartaDungeon
+﻿using System.Numerics;
+
+namespace SpartaDungeon
 {
     internal class Program
     {
@@ -131,6 +133,27 @@
             }
             EquipScreen();
         }
+
+        public void SHopScreen()
+        {
+            Console.Clear();
+            Console.WriteLine("상점");
+            Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
+            Console.WriteLine();
+            Console.WriteLine("[보유 골드]");
+            Console.WriteLine($"/player.Gold G");
+            Console.WriteLine();
+            Console.WriteLine("[아이템 목록]");
+
+            for (int i = 0; i < equipItemList.Count; i++)
+            {
+                equipItemList[i].DisplayShopItem();
+            }
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine();
+        }
+
 
     }
 }
