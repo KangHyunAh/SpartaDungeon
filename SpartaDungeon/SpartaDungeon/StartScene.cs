20571f20112ea91f5f37c6a09aa3950ba9d1ad49
kdh5790 <k_0106@naver.com>
2025-02-06 17:32:27 +0900

    [ Feat ]StartScene 클래스 및 Lobby 생성

---
+++ SpartaDungeon/SpartaDungeon/StartScene.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaDungeon
+{
+    internal class StartScene
+    {
+        public void Lobby()
+        {
+            Console.Clear();
+
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+            Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("1. 캐릭터 정보");
+            Console.WriteLine("2. 인벤토리");
+            Console.WriteLine("3. 상점");
+            Console.WriteLine($"4. 던전 입장(현재 층)"); // (현재 층) 안에 현재 층수 표시
+            Console.WriteLine("5. 휴식하기");
+            Console.WriteLine();
+
+            int selectNumber = Utility.GetInput(1, 5);
+
+            switch (selectNumber)
+            {
+                case 1:
+                    // 캐릭터 정보 이동 함수
+                    break;
+                case 2:
+                    // 인벤토리 이동 함수
+                    break;
+                case 3:
+                    // 상점 이동 함수
+                    break;
+                case 4:
+                    // 던전 이동 함수
+                    break;
+                case 5:
+                    // 휴식 이동 함수
+                    break;
+            }
+        }
+    }
+}
