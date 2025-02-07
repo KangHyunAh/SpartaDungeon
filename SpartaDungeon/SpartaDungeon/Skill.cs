b55d8dbbb1c4a23e6ac14b53c75ba0082cbb53a3
kdh5790 <k_0106@naver.com>
2025-02-07 19:45:49 +0900

    [ Fix ] 스킬 사용 로직 수정
    랜덤 적 공격하는 스킬 사용 시 로직 수정

--- SpartaDungeon/SpartaDungeon/Skill.cs
+++ SpartaDungeon/SpartaDungeon/Skill.cs
@@ -24,7 +24,7 @@ namespace SpartaDungeon
         public int Count { get; }
         public int UseHp { get; }
         public int UseMp { get; }
-        
+
 
         public Skill(string n, string d, SkillType t, float p, int c, int h, int m)
         {
@@ -52,16 +52,24 @@ namespace SpartaDungeon
             else if (Count != 5 && Count != 1)
             {
                 List<Monster> target = monsters.Where(i => i.Health > 0).OfType<Monster>().ToList();
-                if(target.Count == 1)
+                if (target.Count == 1)
                 {
                     target[0].Health -= (int)((player.strikePower + player.equipStrikePower) * SkillPower);
                 }
-                else
+                else if (target.Count >= 2)
                 {
-                    for(int i = 0; i < target.Count; i++)
+                    int rndtargetIndex1 = new Random().Next(0, target.Count);
+
+                    target[rndtargetIndex1].Health -= (int)((player.strikePower + player.equipStrikePower) * SkillPower);
+
+                    int rndtargetIndex2 = new Random().Next(0, target.Count);
+
+                    while (rndtargetIndex1 != rndtargetIndex2)
                     {
-                        target[i].Health -= (int)((player.strikePower + player.equipStrikePower) * SkillPower);
+                        rndtargetIndex2 = new Random().Next(0, target.Count);
                     }
+
+                    target[rndtargetIndex2].Health -= (int)((player.strikePower + player.equipStrikePower) * SkillPower);
                 }
             }
             else
@@ -78,7 +86,7 @@ namespace SpartaDungeon
     {
         public static void SkillInit(Player player)
         {
-            switch(player.chad)
+            switch (player.chad)
             {
                 case "나이트":
                     player.skills.Add(new Skill("휩쓸기", "적 전체에 공격력 * 0.3의 데미지를 준다.", SkillType.Attack, 0.3f, 5, 0, 10));
