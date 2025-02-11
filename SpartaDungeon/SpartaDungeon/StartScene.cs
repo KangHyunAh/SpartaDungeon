a0edd2ed511b94f97eb0ab1541b56d96b9e06a1b
kdh5790 <k_0106@naver.com>
2025-02-11 20:03:21 +0900

    [ Fix ] StartScene 수정
    로비 UI 수정

--- SpartaDungeon/SpartaDungeon/StartScene.cs
+++ SpartaDungeon/SpartaDungeon/StartScene.cs
@@ -26,15 +26,34 @@ namespace SpartaDungeon
 
                 Console.Clear();
 
+
                 Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
                 Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
                 Console.WriteLine();
+
+                Console.WriteLine("┌────────────────────────────────────────────────────┐");
+                Console.WriteLine("│  ####   ####       #      ####     #####      #    │");
+                Console.WriteLine("│ #   #    #  #     ###      #  #    # # #     ###   │");
+                Console.WriteLine("│ #        #  #     # #      #  #      #       # #   │");
+                Console.WriteLine("│  ###     #  #    #  #      ###       #      #  #   │");
+                Console.WriteLine("│     #    ###     #####     #  #      #      #####  │");
+                Console.WriteLine("│ #   #    #       #   #     #  #      #      #   #  │");
+                Console.WriteLine("│ ####    ###     ##   ##   ###  #    ###    ##   ## │");
+                Console.WriteLine("└────────────────────────────────────────────────────┘");
+
+                Console.WriteLine();
                 Console.WriteLine("1. 캐릭터 정보");
+                Console.WriteLine();
                 Console.WriteLine("2. 인벤토리");
+                Console.WriteLine();
                 Console.WriteLine("3. 상점");
+                Console.WriteLine();
                 Console.WriteLine($"4. 던전 입장({gm.player.dungeonLevel} 층)");
+                Console.WriteLine();
                 Console.WriteLine("5. 퀘스트");
+                Console.WriteLine();
                 Console.WriteLine("6. 휴식하기");
+                Console.WriteLine();
                 Console.WriteLine("7. 게임종료");
                 Console.WriteLine();
 
